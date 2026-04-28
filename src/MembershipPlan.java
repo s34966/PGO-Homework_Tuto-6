d1b806dca9fd77e650b9c2aff6cd2678c1160668
Boran Ankarali <boranankarali@BAs-MacBook-M1-M.local>
2026-04-28 11:47:32 +0200

    Add my all files

---
+++ src/MembershipPlan.java
@@ -0,0 +1,63 @@
+public abstract class MembershipPlan implements Billable {
+    private String planCode;
+    private String clientName;
+    private int months;
+    private double baseMonthlyFee;
+    private boolean autoRenew;
+
+    public MembershipPlan(String planCode, String clientName, int months, double baseMonthlyFee, boolean autoRenew) {
+        this.planCode = planCode;
+        this.clientName = clientName;
+        this.months = months;
+        this.baseMonthlyFee = baseMonthlyFee;
+        this.autoRenew = autoRenew;
+    }
+
+    public String getPlanCode() {
+        return planCode;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public double getBaseMonthlyFee() {
+        return baseMonthlyFee;
+    }
+
+    public boolean isAutoRenew() {
+        return autoRenew;
+    }
+
+    public abstract String getPlanType();
+
+    @Override
+    public abstract double calculateMonthlyNetPrice();
+
+    @Override
+    public double calculateMonthlyGrossPrice() {
+        return calculateMonthlyNetPrice() * 1.23;
+    }
+
+    public double calculateTotalNetPrice() {
+        return calculateMonthlyNetPrice() * months;
+    }
+
+    public final void printSummary() {
+        System.out.println("Plan: " + getPlanType() + " [" + planCode + "]");
+        System.out.println("Client: " + clientName);
+        System.out.println("Duration: " + months + " month(s)");
+        System.out.printf("Monthly net price: %.2f PLN%n", calculateMonthlyNetPrice());
+        System.out.printf("Monthly gross price: %.2f PLN%n", calculateMonthlyGrossPrice());
+        System.out.printf("Total contract net: %.2f PLN%n", calculateTotalNetPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "MembershipPlan{planCode='" + planCode + "', clientName='" + clientName + "', months=" + months + ", baseMonthlyFee=" + baseMonthlyFee + ", autoRenew=" + autoRenew + "}";
+    }
+}
